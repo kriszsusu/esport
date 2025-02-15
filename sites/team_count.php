7b6fb68518874348466f3d769403a459ef94422b
Dzsanga <119756283+DzsangaGIT@users.noreply.github.com>
2025-02-15 12:13:24 +0100

    Update team_count.php

--- sites/team_count.php
+++ sites/team_count.php
@@ -1,4 +1,6 @@
-/* <?php
+<?php
+header('Content-Type: application/json');
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,13 +9,15 @@ $dbname = "esport_reg";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo json_encode(["error" => "Database connection failed"]);
+    exit();
 }
 
-$sql = "SELECT COUNT(*) AS count FROM registration";
+$sql = "SELECT COUNT(*) AS count FROM registrations";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 
-echo $row['count'];
+echo json_encode(["count" => $row['count']]);
+
 $conn->close();
-/?>*/ 
+?>
